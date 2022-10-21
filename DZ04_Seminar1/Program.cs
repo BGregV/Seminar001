1446b2b699f2cf645947c3db2ebc8fa0f45d1abc
BGregV <greguar@ninodom.ru>
2022-10-21 12:56:05 +0300

    Четвертое задание готово

---
+++ DZ04_Seminar1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число:");
+int pervoeChislo = int.Parse(Console.ReadLine()?? "0");
+if (pervoeChislo% 2==1)
+{
+    Console.WriteLine ("Число НЕ чётное");
+}
+else
+{
+   Console.WriteLine ("Число чётное");
+}

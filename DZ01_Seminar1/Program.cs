d92c8c200dff3428702b34b2c016d51d8dfd7608
BGregV <greguar@ninodom.ru>
2022-10-21 13:42:48 +0300

    Добавил все файлы с игнором

--- DZ01_Seminar1/Program.cs
+++ DZ01_Seminar1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число:");
+double pervoeChislo = double.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите второе число:");
+double vtoroeChislo = double.Parse(Console.ReadLine()?? "0");
+double kvadrat = Math.Pow(vtoroeChislo, 2);
+if (pervoeChislo == kvadrat)
+{
+    Console.WriteLine ("Первое число квадрат второго");
+}
+else
+{
+    Console.WriteLine ("Первое число не квадрат второго");
+}

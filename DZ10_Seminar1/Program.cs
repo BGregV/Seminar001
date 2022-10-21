49d24c158dc6f93b58d824715f95b38d8888ff94
BGregV <greguar@ninodom.ru>
2022-10-21 13:20:42 +0300

    Деcятое задание готово

---
+++ DZ10_Seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трёхзначное числo:");
+int Chislo = int.Parse(Console.ReadLine() ?? "0");
+int tretiechislo = ((Chislo % 100) % 10);
+int vtoroechislo = ((Chislo % 100) / 10);
+int pervoechislo = (Chislo / 100);
+int res = (pervoechislo * 10) + tretiechislo;
+Console.WriteLine($"первое число {pervoechislo}, второе число {vtoroechislo}, третье число {tretiechislo}");
+Console.WriteLine($"{pervoechislo}{tretiechislo}");
+Console.WriteLine(res);

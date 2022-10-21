e955511324e30c7a5c8637b98f614a9fe421f754
BGregV <greguar@ninodom.ru>
2022-10-21 12:46:58 +0300

    Второе задание

--- DZ02_Seminar1/Program.cs
+++ DZ02_Seminar1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер дня от 1 до 7");
+int DenNedeli = int.Parse(Console.ReadLine()?? "0");
+if (DenNedeli <= 7)
+switch (DenNedeli){
+    case 1:
+    Console.WriteLine("Понедельник");
+    break;
+    case 2:
+    Console.WriteLine("Вторник");
+    break;
+    case 3:
+    Console.WriteLine("Среда");
+    break;
+    case 4:
+    Console.WriteLine("Четверг");
+    break;
+    case 5:
+    Console.WriteLine("Пятница");
+    break;
+    case 6:
+    Console.WriteLine("Суббота");
+    break;
+    case 7:
+    Console.WriteLine("Воскресенье");
+    break;
+}
+else
+{
+Console.WriteLine ("Такого дня не существует");
+}

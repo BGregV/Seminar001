ad50383be36f78090089e9a1cf735e622a0381dc
BGregV <greguar@ninodom.ru>
2022-10-21 12:47:20 +0300

    Третье задание готово

--- DZ03_Seminar1/Program.cs
+++ DZ03_Seminar1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число:");
+double pervoeChislo = double.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите второе число:");
+double vtoroeChislo = double.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите третье число:");
+double tretieChislo = double.Parse(Console.ReadLine()?? "0");
+
+Console.WriteLine (Math.Max(pervoeChislo, Math.Max(vtoroeChislo, tretieChislo)));

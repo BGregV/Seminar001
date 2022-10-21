09c37261a42642b1e1205ac2e164b4928a54e86f
BGregV <greguar@ninodom.ru>
2022-10-21 13:17:33 +0300

    Девятое задание готово

---
+++ DZ09_Seminar1/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 100);//1 2 3 4...7
+Console.WriteLine($"Число {numberA}");
+int vtoroechislo = (numberA % 10);
+int pervoechislo = (numberA / 10);
+Console.WriteLine($"первое число {pervoechislo}, второе число {vtoroechislo}");
+Console.WriteLine("Max: " + Math.Max(pervoechislo, vtoroechislo));

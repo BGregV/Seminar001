8e899d111a6b45bf0924c46b9f18a91426f4313f
BGregV <greguar@ninodom.ru>
2022-10-21 13:28:52 +0300

    Одиннадцатое задание готово

---
+++ DZ11_Seminar1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число:");
+double pervoeChislo = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число:");
+double vtoroeChislo = double.Parse(Console.ReadLine() ?? "0");
+double ostatok = (pervoeChislo % vtoroeChislo);
+if (  pervoeChislo % vtoroeChislo == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления {ostatok}");
+}

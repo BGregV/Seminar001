e269111514809f09f53de0d4d823eab875cf77f5
BGregV <greguar@ninodom.ru>
2022-10-21 12:59:36 +0300

    Пятое задание готово

---
+++ DZ05_Seminar1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число:");
+double Chislo = double.Parse(Console.ReadLine() ?? "0");
+double b = (Chislo * -1);
+if (Chislo > 0)
+{
+    while (b <= Chislo)
+    {
+        Console.Write(" {0}", b);
+        b++;
+    }
+}
+else
+{
+    while (Chislo <= b)
+    {
+        Console.Write(" {0}", Chislo);
+        Chislo++;
+    }
+}
